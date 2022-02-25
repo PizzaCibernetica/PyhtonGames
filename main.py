0468ee610380f299887a048a50e1ac4181932fcf
PizzaCibernetica <mspigolon.cyber@gmail.com>
2022-02-25 11:23:09 -0500

    limit movement of rockets

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
 # This is my first Python Game
+from termios import VEOL
 import pygame
 import sys
 import os       # to define path to import hte images
 
-
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 400
 BLUE_COLOR = (0,100,200)        # constant for color 
+BLACK_COLOR = (0,0,0)           # constant for black color
 FPS = 60                        # constant for refresh rate 
+BORDER = pygame.Rect(SCREEN_WIDTH/2-5 ,0 , 10 , SCREEN_HEIGHT)         # position a rectagle to limit movement of rockets
 # ROCKET = pygame.image.load('Assets/rocket.png')  this is the direct refernce 
 ROCKET = pygame.image.load(os.path.join('Assets', 'rocket.png')) # this way it will ensure cross compatibility with different OSs
 ROCKET_H, ROCKET_W = 40 ,60
 ROCKET_RIGHT = pygame.transform.rotate(pygame.transform.scale(ROCKET, (ROCKET_H,ROCKET_W)), 90)
-
 ROCKET_LEFT = pygame.transform.rotate(pygame.transform.scale(ROCKET, (ROCKET_H,ROCKET_W)), 270)
 VELOCITY = 5
 
 pygame.init()
 
-window = pygame.display.set_mode((1000,400))
+window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("My first Game")
 
 
 # defining the draw window
 def draw_window(left_rocket,right_rocket):
     window.fill(BLUE_COLOR)     # draw this first for avoind covering the rest
+    pygame.draw.rect(window, BLACK_COLOR, BORDER)
     window.blit(ROCKET_RIGHT, (right_rocket.x,right_rocket.y))         # blit is a surface on top of the screen
     window.blit(ROCKET_LEFT, (left_rocket.x,left_rocket.y))
     pygame.display.update()         # update an area of the screen if given coordinates
     # or pygame.display.flip()      # refresh the entire screen
 
 def right_rocket_movement(keys_pressed, rocket):
-    if keys_pressed[pygame.K_LEFT]:
+    if keys_pressed[pygame.K_LEFT] and rocket.x - VELOCITY > (SCREEN_WIDTH/2):
             rocket.x -= VELOCITY
-    if keys_pressed[pygame.K_RIGHT]:
+    if keys_pressed[pygame.K_RIGHT] and rocket.x + VELOCITY < (SCREEN_WIDTH - ROCKET_W):
             rocket.x += VELOCITY
-    if keys_pressed[pygame.K_DOWN]:
+    if keys_pressed[pygame.K_DOWN] and rocket.y + VELOCITY < (SCREEN_HEIGHT - ROCKET_H):
             rocket.y += VELOCITY
-    if keys_pressed[pygame.K_UP]:
+    if keys_pressed[pygame.K_UP] and rocket.y - VELOCITY > 0:
             rocket.y -= VELOCITY
 
 def left_rocket_movement(keys_pressed, rocket):
-    if keys_pressed[pygame.K_a]:
+    if keys_pressed[pygame.K_a] and rocket.x - VELOCITY > 0:
             rocket.x -= VELOCITY
-    if keys_pressed[pygame.K_d]:
+    if keys_pressed[pygame.K_d] and rocket.x + VELOCITY < (SCREEN_WIDTH/2 - ROCKET_W):
             rocket.x += VELOCITY
-    if keys_pressed[pygame.K_s]:
+    if keys_pressed[pygame.K_s] and rocket.y + VELOCITY < (SCREEN_HEIGHT - ROCKET_H):
             rocket.y += VELOCITY
-    if keys_pressed[pygame.K_w]:
+    if keys_pressed[pygame.K_w] and rocket.y - VELOCITY > 0:
             rocket.y -= VELOCITY
 
 
